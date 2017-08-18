0e9c9f5b2349c43baa2edf59e0336612244a7a32
Fernanda Mora <30726423+mfpiracocam@users.noreply.github.com>
2017-08-17 19:44:37 -0700

    Update and rename xd.cpp to B.cpp

---
+++ B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main (void)
+{ 
+  std::cout<<"*****"<< "\n";
+  std::cout<<"*    *"<< "\n";
+  std::cout<<"*    *"<< "\n";
+  std::cout<<"*****"<< "\n";
+  std::cout<<"*    *"<< "\n";
+  std::cout<<"*    *"<< "\n";
+  std::cout<<"*****"<< "\n";
+  return 0;
+    }
