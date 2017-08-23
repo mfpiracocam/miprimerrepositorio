db878413db4f385ae747cd9735b7e5641e91008d
Fernanda Mora <mfpiracocam@unal.edu.co>
2017-08-22 22:53:35 -0700

    2.0

--- 2017-08-16/exer6.cpp
+++ 2017-08-16/exer6.cpp
@@ -6,11 +6,50 @@ int main(void)
   std::cout<<"Programa que ordena de mayor a menor 3 enteros"<<"\n";
   std::cout<<"Ingrese los números a ordenar: " << "\n";
   std::cin>> a >> b >> c;
-  if (a>=b && b>=c) {std::cout<<a,b,c;}
+  if (a>b && a>c)
+    {
+      std::cout<<a<<",";
+      if (b>c)
+	{
+	  std::cout<<b<<","<<c<<".";
+	}
+      else
+	{
+	  std::cout<<c<<","<<b<<".";
+	}
+    }
+ else  if (b>a && b>c)
+   {
+     std::cout<<b<<",";
+     if (a>c)
+       {
+	 std::cout<<a<<","<<c<<".";
+       }
+     else
+       {
+	 std::cout<<c<<","<<a<<".";
+       }
+   }
+else
+  {
+    std::cout<<c<<",";
+    if (a>b)
+      {
+	std::cout<<a<<","<<b<<".";
+      }
+    else
+      {
+	std::cout<<b<<","<<a<<".";
+      }
+    
+  }
+  std::cout<<"\n";
+      
+  /*if (a>=b && b>=c) {std::cout<<a,b,c;}
   if (a>=c>=b) {std::cout<<a,c,b;}
   if (b>=a>=c) {std::cout<<b,a,c;}
   if (b>=c>=a) {std::cout<<b,c,a;}
   if (c>=a>=b) {std::cout<<c,a,b;}
-  if (c>=b && b>=a) {std::cout<<c << "  " << b << "  " << a;}
+  if (c>=b && b>=a) {std::cout<<c << "  " << b << "  " << a;}*/
   return 0;
 }
